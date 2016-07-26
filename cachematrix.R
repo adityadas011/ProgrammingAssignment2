0bb58b538af192b21cdcf3a87b1fc7066f9d1f9f
Aditya <das13@purdue.edu>
2016-07-27 01:29:27 +0530

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Calculates inverse of matrix. Uses cached mean if inverse is already computed.
 
-## Write a short comment describing this function
+## Creates a matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y) {
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    set_inverse <- function(inverse) i <<- inverse
+    get_inverse <- function() i
+    list(set=set, get=get, set_inverse=set_inverse, get_inverse=get_inverse)
 }
 
-
-## Write a short comment describing this function
+## Calculates the inverse of the matrix. If mean is calculated it gets the mean from cache and skips computation.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    i <- x$get_inverse()
+    if(!is.null(i)) {
+        message("getting cashed data")
+        return(i)
+    }
+    i <- solve(x$get(), ...)
+    x$set_inverse(i)
+    i
 }
+
+
